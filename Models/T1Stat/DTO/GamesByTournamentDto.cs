5936748f161ba01719852161bba23388a5a9e828
Call me Kaneki <giangkirito11@gmail.com>
2026-03-22 12:29:45 +0700

    Lưu thay đổi trước khi chuyển nhánh

---
+++ Models/T1Stat/DTO/GamesByTournamentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace T1EsportsWeb.Models.T1Stat.DTO
+{
+    public class GamesByTournamentDto
+    {
+        public int IdTournament { get; set; } 
+        public string TournamentName { get; set; }
+        public int Year { get; set; }
+        public string Region { get; set; }
+        public int GamesWon { get; set; }
+        public int GamesLost { get; set; }
+        public double WinRate { get; set; }
+
+        public int GamesKRWon { get; set; }
+        public int GamesKRLost { get; set; }
+        public int GamesINTWon { get; set; }
+        public int GamesINTLost { get; set; }
+    }
+}
